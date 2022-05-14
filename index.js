96bcf76773bbf4a6bdc1d6e161c0b90b0095fb9b
Javier <javier.sardinas@gmail.com>
2022-05-13 23:00:00 -0500

    Add static README content

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const md = require('./md');
 
 
+
+questions = [];
+
+inquirer.prompt(questions)
+.then(answers => {
+    const content = md.generateMd(answers);
+    fs.writeFile('./output/README.md', content, err=>{});
+});
---
+++ md.js
@@ -0,0 +1,17 @@
+function generateMd(a){
+    return `# Project Name
+## Description
+## Table of Contents
+## Installation
+## Usage
+## License
+## Contributing
+## Tests
+## Questions
+`;
+}
+
+
+
+
+module.exports = {generateMd};

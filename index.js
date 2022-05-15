0a04285730cd9f805aeeb1e0258d812c76401027
Javier <javier.sardinas@gmail.com>
2022-05-14 22:49:12 -0500

    Add links to md

--- index.js
+++ index.js
@@ -3,11 +3,69 @@ const fs = require('fs');
 const md = require('./md');
 
 
-
-questions = [];
+questions = [
+    {
+        name: 'projectTitle',
+        message: 'What is the title of your project?'
+    },
+    {
+        name: 'description',
+        message: 'Write a brief description of your project:'
+    },
+    {
+        name: 'installation',
+        message: 'Describe the instalation process:'
+    },
+    {
+        name: 'usage',
+        message: 'Type in a description and examples on how to use your project:'
+    },
+    {
+        name: 'contribution',
+        message: 'How can others contribute to this project?'
+    },
+    {
+        name: 'tests',
+        message: 'Test performed:'
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Questions:"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email: "
+    }
+];
 
 inquirer.prompt(questions)
 .then(answers => {
     const content = md.generateMd(answers);
-    fs.writeFile('./output/README.md', content, err=>{});
+    fs.writeFile('./output/README.md', content, e=>{
+        if (e)
+            console.log(e);
+        else
+            console.log('README.md generated successfully')
+    });
 });
--- md.js
+++ md.js
@@ -1,13 +1,42 @@
-function generateMd(a){
-    return `# Project Name
+function generateMd(answers){
+    return `# ${answers.projectTitle}
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+
 ## Description
+${answers.description}
+
 ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
 ## Installation
+${answers.installation}
+
 ## Usage
+${answers.usage}
+
 ## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+This application is covered under ${answers.license} license.
+
 ## Contributing
+${answers.contribution}
+
 ## Tests
+${answers.tests}
+
 ## Questions
+${answers.questions}
+
+Contact Information
+
+[![](http://www.github.com/${answers.username}.png?size=36) ${answers.username}](http://github.com/${answers.username}) on Github 
+
+[:email: ${answers.email}](mailto:${answers.email})
 `;
 }
 
